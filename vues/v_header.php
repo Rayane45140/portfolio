dc46ea13df55a82cff350847cef28b230dbb0fb0
Rayane <vds.rayane45@gmail.com>
2023-04-03 09:23:23 +0200

    Modification du header

--- vues/v_header.php
+++ vues/v_header.php
@@ -3,7 +3,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    
     <link rel="stylesheet" href="assets/css/portfolio.css">
 </head>
 
@@ -11,6 +13,7 @@
 
 
 <body>
+	<!--
 <div class="container">
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
       <a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
@@ -25,3 +28,22 @@
       </ul>
     </header>
   </div>
+-->
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a href="index.php" class="navbar-brand d-flex align-items-center mb-3 ms-md-5 ms-sm-0 me-auto text-dark text-decoration-none">
+        <span class="fs-4">Van Der Stuyft Rayane</span>
+      </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="ms-auto me-5 navbar-nav mb-2 mb-lg-0">
+        <li class="nav-item"><a href="index.php?uc=competences" class="nav-link">Compétences</a></li>
+        <li class="nav-item"><a href="index.php" class="nav-link">Veille</a></li>
+        <li class="nav-item"><a href="index.php" class="nav-link">Épreuve E4</a></li>
+        <li class="nav-item"><a href="index.php" class="nav-link">Contact</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
